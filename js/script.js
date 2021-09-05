d7a27d345ad93d2ccf7ef793541da069175ae817
Ivanpepp <nastroivan3@gmail.com>
2021-09-05 16:51:18 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -6,27 +6,56 @@ for (let i = 0; i < 5; i++){
     }while(listaNumeri[i] != numeroRandom);
 }
 alert(listaNumeri);
-let listaScelte = [];
-setTimeout(timer(listaScelte), 30000);
-let k = 0;
-let numeriEsatti =[];
-let y;
-for (let i = 0; i < 5; i++){
-    y = listaScelte[i];
-    if(listaNumeri[i] == y){
-        k = k+1;
-        numeriEsatti.push(y)
-    }
-}
-
-console.log('Hai azzecato ' + k + ' Numeri, i seguenti: ' + numeriEsatti);
-
-function timer(arr){
+setTimeout(function(){
+    let listaScelte = [];
     for(let i = 0; i < 5; i++){
-        arr.push(prompt('inserisci numero'));
+        let numeroScelto= parseInt(prompt('inserisci un numero compreso fra 1 e 100'));
+        while(numeroScelto < 0 || numeroScelto > 100 || isNaN(numeroScelto)){
+            numeroScelto= parseInt(prompt('inserisci un numero compreso fra 1 e 100'));
+            if(isNaN(numeroScelto)){
+                alert(numeroScelto + ' non e` un numero, inserisci un numero compreso tra 1 e 100');
+            }else if(numeroScelto < 0 || numeroScelto > 100){
+                alert(numeroScelto + ' non hai in serito un numero valido, inserisci un numero compreso fra 1 e 100');
+            }
+        }
+        listaScelte.push(numeroScelto);  
     }
-    return arr;
-}
+    let numeriEsatti =[];
+    let y;
+    for (let i = 0; i < 5; i++){
+        y = listaScelte[i];
+        if(listaNumeri.includes(y)){
+        numeriEsatti.push(y);
+        }
+    }
+    return console.log('Hai azzecato ' + numeriEsatti.length + ' Numeri, i seguenti: ' + numeriEsatti);
+}, 30000);
+/* function timer(){
+    let listaScelte = [];
+    for(let i = 0; i < 5; i++){
+        let numeroScelto= parseInt(prompt('inserisci un numero compreso fra 1 e 100'));
+        while(numeroScelto < 0 || numeroScelto > 100 || isNaN(numeroScelto)){
+            numeroScelto= parseInt(prompt('inserisci un numero compreso fra 1 e 100'));
+            if(isNaN(listaScelte[i])){
+                alert(listaScelte[i] + ' non e` un numero, inserisci un numero compreso tra 1 e 100');
+            }else if(listaScelte[i] < 0 || listaScelte[i] > 100){
+                alert(listaScelte[i] + ' non hai in serito un numero valido, inserisci un numero compreso fra 1 e 100');
+            }
+        }
+        listaScelte.push(numeroScelto);  
+    }
+    let k = 0;
+    let numeriEsatti =[];
+    let y;
+    for (let i = 0; i < 5; i++){
+        y = listaScelte[i];
+        if(listaNumeri[i] == y){
+        k = k+1;
+        numeriEsatti.push(y);
+    }
+    return console.log('Hai azzecato ' + k + ' Numeri, i seguenti: ' + numeriEsatti);
+    }
+} */
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
